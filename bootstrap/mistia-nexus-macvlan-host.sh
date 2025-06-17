361df9be5483eddb6205d587df51dbe3e53fd54c
Mike Han <mike@alrightylabs.com>
2025-06-18 07:15:31 +0800

    🐛 fix(script): add sudo to network commands for proper execution

--- bootstrap/mistia-nexus-macvlan-host.sh
+++ bootstrap/mistia-nexus-macvlan-host.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ip link add macvlan-host link bridge0 type macvlan mode bridge
-ip addr add 192.168.50.3/24 dev macvlan-host
-ip link set macvlan-host up
-ip route add 192.168.50.2/32 dev macvlan-host
+sudo ip link add macvlan-host link bridge0 type macvlan mode bridge
+sudo ip addr add 192.168.50.3/24 dev macvlan-host
+sudo ip link set macvlan-host up
+sudo ip route add 192.168.50.2/32 dev macvlan-host
